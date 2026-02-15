ecd92eea8002e0414c24a2d8bddc2e89ecfe8481
pblo <pdiazarenas541@gmail.com>
2026-02-15 05:22:14 +0000

    first sketch, without models and controllers 2

--- server/database/database.js
+++ server/database/database.js
@@ -1,21 +1,65 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import {Pool} from 'pg';
-import format from 'pg-format' 
+import dotenv from "dotenv";
+dotenv.config();
+import { Pool } from "pg";
+import bcrypt from "bcryptjs";
+import format from "pg-format";
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
-    allowExitOnIdle:true
-})
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  allowExitOnIdle: true,
+});
 
-export const getDataConnection = async()=>{
-    const {rows} = await pool.query("SELECT NOW()")
-    if(!rows || rows.length === 0){
-        console.log(error)
+export const getDataConnection = async () => {
+  const { rows } = await pool.query("SELECT NOW()");
+  if (!rows || rows.length === 0) {
+    console.log(error);
+  }
+  console.log(`Database connected at ${rows[0].now}`);
+};
+
+export const getUser = async (email) => {
+  try {
+    const query = "SELECT * from usuarios WHERE email = $1";
+    const { rows, rowCount} = await pool.query(query, [email]);
+    if(!rowCount){
+        throw {code: 404, message: "User not found"}
     }
-    console.log(`Database connected at ${rows[0].now}`)
-}
+    return rows;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const createUSer = async (usuario) => {
+  // crear varaible usuario
+  let { email, password } = usuario;
+  password = password.toString();
+  //encriptar clave
+  const encryptPass = bcrypt.hashSync(password);
+  //crear array de valores para consulta
+  const values = [email, encryptPass];
+  //crear consulta
+  const query = "INSERT INTO usuarios values (DEFAULT, $1, $2)";
+  await pool.query(query, values);
+};
+
+export const verifyUser = async (email, password) => {
+  const values = [email];
+  const query = "SELECT * from usuarios where email = $1";
+  const {
+    rows: [usuario],
+    rowCount,
+  } = await pool.query(query, values);
+  const { password: encryptPass } = usuario;
+  const rightPass = bcrypt.compareSync(password, encryptPass);
+
+  if (!rightPass || !rowCount)
+    throw {
+      code: 404,
+      message: "Email o contraseña incorrecta",
+    };
+};
--- server/index.js
+++ server/index.js
@@ -2,24 +2,69 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
+
 import cors from "cors";
 
-import { getDataConnection } from "./database/database.js";
+import { getDataConnection, getUser } from "./database/database.js";
+import { verifyUser, createUSer } from "./database/database.js";
+import { reportQuery } from "./lib/tokenMiddleware.js";
 
 const app = express();
-const PORT = process.env.API_PORT
+const PORT = process.env.API_PORT;
+const SECRET_KEY = process.env.SUPER_SECRET;
 
 app.use(express.json());
 app.use(cors());
 
+app.get("/", async (req, res) => {
+  await getDataConnection();
+  res.send("Page working");
+});
 
-app.get('/', async (req,res)=>{
-    await getDataConnection()
-    res.send('Page working')
-})
+app.get("/usuarios",reportQuery, async (req, res) => {
+  try {
+    const Authorization = req.header("Authorization");
+    const token = Authorization.split("Bearer ")[1];
+    //verificar
+    jwt.verify(token, `${SECRET_KEY}`);
+    //ejectuar acción autorizada
+    
+    //decode email
+    const {email} = jwt.decode(token)
+    console.log(`user ${email} has accessed to his information via getUser`)
+    
+    const payload = await getUser(email);
+    return res.status(200).json(payload);
+  } catch (error) {
+    console.error(error);
+    return res.status(error.code || 500).send(error)
+  }
+});
 
-app.listen(PORT, async ()=>{
-    console.log(`page working on http://localhost:${PORT}`)
-    await getDataConnection()
-})
+app.post("/login",reportQuery, async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    await verifyUser(email, password);
+    const token = jwt.sign({ email }, `${SECRET_KEY}`);
+    res.send(token);
+  } catch (error) {
+    console.error(error);
+    res.status(error.code || 500).send(error);
+  }
+});
+
+app.post("/usuarios",reportQuery, async (req, res) => {
+  try {
+    const usuario = req.body;
+    await createUSer(usuario);
+    res.status(201).send("Usuario cread con exito");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
+
+app.listen(PORT, async () => {
+  console.log(`page working on http://localhost:${PORT}`);
+  await getDataConnection();
+});
