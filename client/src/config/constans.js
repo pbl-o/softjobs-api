08c5989fc9bf86b98019a1e2bd65f31a6b3e1f8e
pblo <pdiazarenas541@gmail.com>
2026-02-15 23:21:30 +0000

    add comment

--- client/src/config/constans.js
+++ client/src/config/constans.js
@@ -4,3 +4,12 @@ export const ENDPOINT = {
   login: `${URLBASE}/usuarios/login`,
   users: `${URLBASE}/usuarios`
 }
+
+
+/* 
+
+Modifiqué la ruta del login 
+para poder colocar las tres rutas del backend 
+bajo el mismo router 
+
+*/
